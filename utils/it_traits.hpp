469261746c79d44f22b1f8cd722f9cf8b668cc78
Askeladd42 <piema.lam@gmail.com>
2022-12-07 17:30:23 +0100

    readded the iterator and functions associated templates

--- utils/it_traits.hpp
+++ utils/it_traits.hpp
@@ -6,11 +6,92 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 10:37:18 by plam              #+#    #+#             */
-/*   Updated: 2022/12/07 10:37:58 by plam             ###   ########.fr       */
+/*   Updated: 2022/12/07 15:36:49 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef
-# define
+#ifndef __IT_TRAITS_HPP__
+# define __IT_TRAITS_HPP__
+
+namespace ft {
+	template<bool B, class T = void>
+	struct enable_if { };
+
+	template<class T>
+	struct enable_if<true, T> {
+		typedef T type;
+	};
+
+	template<class T, T v>
+	struct integral_constant {
+		static const T					value = v;
+		typedef T						value_type;
+		typedef integral_constant<T, v>	type;
+	};
+
+	struct true_type : ft::integral_constant<bool, true> {};	// true_type : struct that return true for the type T associated
+	struct false_type : ft::integral_constant<bool, false> {};	// false_type: struct that return false for the type T associated
+
+	template<typename T>
+	struct remove_cv
+	{ typedef T type; };
+
+	template<typename T>
+	struct remove_cv<const T>
+	{ typedef T type; };
+
+	template<typename T>
+	struct remove_cv<volatile T>
+	{ typedef T type; };
+
+	template<typename T>
+	struct remove_cv<const volatile T>
+	{ typedef T type; };
+
+	template<typename>
+	struct _is_integral_helper : public ft::false_type {};
+
+	template<>
+	struct _is_integral_helper<bool> : public ft::true_type {};
+
+	template<>
+	struct _is_integral_helper<char> : public ft::true_type {};
+
+	template<>
+	struct _is_integral_helper<wchar_t> : public ft::true_type {};
+
+	template<>
+	struct _is_integral_helper<signed char> : public ft::true_type {};
+
+	template<>
+	struct _is_integral_helper<short int> : public ft::true_type {};
+
+	template<>
+	struct _is_integral_helper<int> : public ft::true_type {};
+
+	template<>
+	struct _is_integral_helper<long int> : public ft::true_type {};
+
+	template<>
+	struct _is_integral_helper<long long int> : public ft::true_type {};
+
+	template<>
+	struct _is_integral_helper<unsigned char> : public ft::true_type {};
+
+	template<>
+	struct _is_integral_helper<unsigned short int> : public ft::true_type {};
+
+	template<>
+	struct _is_integral_helper<unsigned int> : public ft::true_type {};
+
+	template<>
+	struct _is_integral_helper<unsigned long int> : public ft::true_type {};
+
+	template<>
+	struct _is_integral_helper<unsigned long long int> : public ft::true_type {};
+
+	template<typename T>
+	struct is_integral : public ft::_is_integral_helper<typename ft::remove_cv<T>::type>::type {};	
+}
 
 #endif
--- utils/iterator.hpp
+++ utils/iterator.hpp
@@ -6,11 +6,27 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 10:37:44 by plam              #+#    #+#             */
-/*   Updated: 2022/12/07 10:38:03 by plam             ###   ########.fr       */
+/*   Updated: 2022/12/07 17:29:37 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef
-# define
+#ifndef __ITERATOR_HPP__
+# define __ITERATOR_HPP__
+
+
+# include <stddef.h>
+# include <iterator>
+# include "it_traits.hpp"
+
+namespace ft {
+	template<typename Category, typename T, typename Distance = ptrdiff_t , typename Pointer = T*, typename Reference = T&>
+	struct iterator {
+		typedef Category	iterator_category;
+		typedef T			value_type;
+		typedef Distance	difference_type;
+		typedef Pointer		pointer;
+		typedef Reference	reference;
+	};
+}
 
 #endif
