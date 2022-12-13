7bafc7fd1468f037d7c5bc1c7e0e5e2785b2f60e
Pierre-marie LAM <plam@e2r10p16.clusters.42paris.fr>
2022-12-13 12:38:45 +0100

    added iterator and iterator_traits headers recreation to the repo

--- utils/iterator.hpp
+++ utils/iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 10:37:44 by plam              #+#    #+#             */
-/*   Updated: 2022/12/07 17:29:37 by plam             ###   ########.fr       */
+/*   Updated: 2022/12/13 12:38:34 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,40 @@ namespace ft {
 		typedef Distance	difference_type;
 		typedef Pointer		pointer;
 		typedef Reference	reference;
+	};
+
+/* iterator_traits from it_traits.hpp
+** reproducing the std::iterator_traits in the STL library :
+** https://en.cppreference.com/w/cpp/iterator/iterator_traits
+*/
+
+	template<typename Iterator>
+	struct iterator_traits{
+		typedef typename Iterator::iterator_category	iterator_category;
+		typedef typename Iterator::value_type			value_type;
+		typedef typename Iterator::difference_type		difference_type;
+		typedef typename Iterator::pointer				pointer;
+		typedef typename Iterator::reference			reference;
+	};
+
+	template<typename T>
+	struct iterator_traits<T*> {
+		typedef typename		iterator_category;
+		typedef typename Iterator::value_type			value_type;
+		typedef typename Iterator::difference_type		difference_type;
+		typedef typename Iterator::pointer				pointer;
+		typedef typename Iterator::reference			reference;
+	};
+
+	template<typename T>
+	struct iterator_traits<const T*> {
+
+		typedef typename		iterator_category;
+		typedef typename Iterator::value_type			value_type;
+		typedef typename Iterator::difference_type		difference_type;
+		typedef typename Iterator::pointer				pointer;
+		typedef typename Iterator::reference			reference;
+
 	};
 }
 
