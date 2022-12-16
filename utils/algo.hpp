8f57186c8e43d81aa0baad2138927b070a1a91c6
Pierre-marie LAM <plam@e1r2p10.clusters.42paris.fr>
2022-12-16 15:55:12 +0100

    recreating arithmetic operator functions for reverse_iterators

--- utils/algo.hpp
+++ utils/algo.hpp
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vector.hpp                                         :+:      :+:    :+:   */
+/*   algo.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/18 18:32:11 by plam              #+#    #+#             */
-/*   Updated: 2022/12/16 15:54:15 by plam             ###   ########.fr       */
+/*   Created: 2022/12/16 15:54:45 by plam              #+#    #+#             */
+/*   Updated: 2022/12/16 15:55:09 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __VECTOR_TPP__
-# define __VECTOR_TPP__
-
-# include <iostream>
 # include <functional>
 # include <memory> 
 
-template< typename T >
-class vector {
-	protected:
-		size_t	_size;
-	public:
-		vector();
-		vector(size_t size);
-		vector(const vector &other);
-		~vector();
-};
-
-#endif
+namespace ft {
+	
+}
