9d28f7d46c14c4ff16d295603db27a1f4d7a952d
Pierre-marie LAM <plam@e1r2p12.clusters.42paris.fr>
2022-12-17 16:23:32 +0100

    iterators finished, need tests with vector when finished, vector started

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 18:32:11 by plam              #+#    #+#             */
-/*   Updated: 2022/12/16 16:02:05 by plam             ###   ########.fr       */
+/*   Updated: 2022/12/17 16:22:59 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,33 @@
 # include <stddef.h>
 # include <functional>
 # include <memory>
+# include <string>
+# include <sstream>
 # include "../utils/it_traits.hpp"
 # include "../utils/iterator.hpp"
 # include "../utils/algo.hpp"
 
-template< typename T >
-class vector {
-	protected:
-		size_t	_size;
-	public:
-		vector();
-		vector(size_t size);
-		vector(const vector &other);
-		~vector();
-};
+/* Vector container header recreation
+** ressource : https://cplusplus.com/reference/vector/vector/
+*/
+
+namespace ft {
+	template< class T, class Alloc = std::allocator<T> >
+	class vector {
+		public:
+			typedef std::size_t									size_type;
+			typedef std::ptrdiff_t								difference_type;
+			typedef T											value_type;
+			typedef Alloc<value_type>							allocator_type;
+			typedef typename allocator_type::reference			reference;
+			typedef typename allocator_type::reference const	const_reference;
+			typedef typename allocator_type::pointer			pointer;
+			typedef typename allocator_type::pointer const		const_pointer;
+			typedef typename ft::normal_iterator				iterator;
+			typedef typename ft::normal_iterator const			const_iterator;
+			typedef typename ft::reverse_iterator				reverse_iterator;
+			typedef typename ft::reverse_iterator const			const_reverse_iterator;
+	};
+}
 
 #endif
