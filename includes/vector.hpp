6f7bfa3707097d29edf541c031b74172824133ce
Pierre-marie LAM <plam@e1r2p12.clusters.42paris.fr>
2022-12-17 16:29:51 +0100

    iterators finished, need tests with vector when finished, vector started

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 18:32:11 by plam              #+#    #+#             */
-/*   Updated: 2022/12/17 16:22:59 by plam             ###   ########.fr       */
+/*   Updated: 2022/12/17 16:29:07 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,18 +33,18 @@ namespace ft {
 	template< class T, class Alloc = std::allocator<T> >
 	class vector {
 		public:
-			typedef std::size_t									size_type;
-			typedef std::ptrdiff_t								difference_type;
-			typedef T											value_type;
-			typedef Alloc<value_type>							allocator_type;
-			typedef typename allocator_type::reference			reference;
-			typedef typename allocator_type::reference const	const_reference;
-			typedef typename allocator_type::pointer			pointer;
-			typedef typename allocator_type::pointer const		const_pointer;
-			typedef typename ft::normal_iterator				iterator;
-			typedef typename ft::normal_iterator const			const_iterator;
-			typedef typename ft::reverse_iterator				reverse_iterator;
-			typedef typename ft::reverse_iterator const			const_reverse_iterator;
+			typedef std::size_t										size_type;
+			typedef std::ptrdiff_t									difference_type;
+			typedef T												value_type;
+			typedef Alloc<value_type>								allocator_type;
+			typedef typename allocator_type::reference				reference;
+			typedef typename allocator_type::const_reference		const_reference;
+			typedef typename allocator_type::pointer				pointer;
+			typedef typename allocator_type::const_pointer			const_pointer;
+			typedef typename ft::normal_iterator<pointer>			iterator;
+			typedef typename ft::normal_iterator<const_pointer>		const_iterator;
+			typedef typename ft::reverse_iterator<iterator>			reverse_iterator;
+			typedef typename ft::reverse_iterator<const_iterator>	const_reverse_iterator;
 	};
 }
 
