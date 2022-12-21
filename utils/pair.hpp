b53af4d75f82f738b56e7a70558ea6594279a657
Pierre-marie LAM <plam@e1r10p8.clusters.42paris.fr>
2022-12-21 15:10:56 +0100

    iterators finished, need tests with vector when finished, vector started

--- utils/pair.hpp
+++ utils/pair.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 14:51:17 by plam              #+#    #+#             */
-/*   Updated: 2022/12/21 07:48:07 by plam             ###   ########.fr       */
+/*   Updated: 2022/12/21 15:10:45 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,75 +16,58 @@
 #include "iterator.hpp"
 
 namespace ft {
-/**
-	 * Pair of values
-	 * 
-	 * This class couples together a pair of values, which may be of different types (T1 and T2).
-	 * The individual values can be accessed through its public members first and second.
-	 * 
-	 * Pairs are a particular case of tuple.
-	 * 
-	 */
+/* Pair of values :
+** 
+** This class couples together a pair of values, which may be of different types (T1 and T2).
+** The individual values can be accessed through its public members first and second.
+** 
+** Pairs are a particular case of tuple.
+*/
 	template<class T1, class T2>
 	struct pair {
-		typedef T1 first_type;
-		typedef T2 second_type;
+		typedef T1	first_type;
+		typedef T2	second_type;
 
-		first_type first;
-		second_type second;
+		first_type	first;
+		second_type	second;
 
-		/**
-		 * (1) default constructor
-		 * 
-		 * Constructs a pair object with its elements value-initialized.
-		 * 
-		 */
+	/* (1) default constructor
+	** Constructs a pair object with its elements value-initialized.
+	*/
 		pair() : first(), second() { }
 
-		/**
-		 * (2) copy / move constructor (and implicit conversion)
-		 * 
-		 * The object is initialized with the contents of the pr pair object.
-		 * 
-		 * The corresponding member of pr is passed to the constructor of each of its members.
-		 * 
-		 */
+	/*(2) copy / move constructor (and implicit conversion) :
+	** The object is initialized with the contents of the pr pair object.
+	** The corresponding member of pr is passed to the constructor of each of its members.
+	*/
 		template<class U, class V>
 		pair(const pair<U, V> &pr) : first( pr.first ), second( pr.second ) { }
 
-		/**
-		 * (3) initialization constructor
-		 * 
-		 * Member first is constructed with a and member second with b.
-		 * 
-		 */
+	/* (3) initialization constructor :
+	** Member first is constructed with a and member second with b.
+	*/
 		pair(const first_type &a, const second_type &b) : first( a ), second( b ) { }
+		
+	/* Destructor function */
+		~pair() { }
 
-		/**
-		 * Assign contents
-		 * Assigns pr as the new content for the pair object.
-		 * 
-		 * Member first is assigned pr.first, and member second is assigned pr.second.
-		 * 
-		 * The copying forms (1) perform copy assignments, with the elements of its argument 
-		 * preserving their values after the call. While the moving forms (2) perform move 
-		 * assignments (as if calling forward for each element), which, for elements of types
-		 * supporting move semantics implies that these elements of pr are left in an unspecified but valid state.
-		 * 
-		 */
-		pair &operator=(const pair &pr){
-			if ( &pr == this )
+	/* Assign contents :
+	** Assigns pr as the new content for the pair object.
+	** 
+	** Member first is assigned pr.first, and member second is assigned pr.second.
+	** 
+	** The copying forms (1) perform copy assignments, with the elements of its argument 
+	** preserving their values after the call. While the moving forms (2) perform move 
+	** assignments (as if calling forward for each element), which, for elements of types
+	** supporting move semantics implies that these elements of pr are left in an unspecified but valid state.
+	*/
+		pair	&operator=(const pair &pr) {
+			if (&pr == this)
 				return *this;
 			first = pr.first;
 			second = pr.second;
 			return *this;
 		}
-
-		/**
-		 * Destroy the pair object
-		 * 
-		 */
-		~pair() { }
 	};
 
 	template<class T1, class T2>
@@ -118,17 +101,12 @@ namespace ft {
 	}
 
 
-	/**
-	 * Construct pair object
-	 * 
-	 * Constructs a pair object with its first element set to x and its second element set to y.
-	 * 
-	 * The template types can be implicitly deduced from the arguments passed to make_pair.
-	 * 
-	 * pair objects can be constructed from other pair objects containing different types, 
-	 * if the respective types are implicitly convertible.
-	 * 
-	 */
+	/* Construct pair object :
+	** Constructs a pair object with its first element set to x and its second element set to y.
+	** The template types can be implicitly deduced from the arguments passed to make_pair.
+	** Pair objects can be constructed from other pair objects containing different types, 
+	** if the respective types are implicitly convertible.
+	*/
 	template<class T1, class T2>
 	pair<T1, T2> make_pair(T1 x, T2 y){
 		return pair<T1, T2>(x, y);
