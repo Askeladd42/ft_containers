96b90a0e77d3ccbc2a02c902243f0dbb28d8a9e5
Askeladd42 <piema.lam@gmail.com>
2022-12-26 10:54:30 +0100

    pair & algorithm added, stack started

--- includes/stack.hpp
+++ includes/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 18:54:29 by plam              #+#    #+#             */
-/*   Updated: 2022/12/21 18:44:13 by plam             ###   ########.fr       */
+/*   Updated: 2022/12/24 00:22:39 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,146 @@
 # include "vector.hpp"
 
 namespace ft {
-template< class T1, class T2 >
-class stack : public vector {
-	private:
-		/* data */
-	public:
-		stack(/* args */);
-		~stack();
+	/* Stack container implementation :
+	** ressource : https://www.cplusplus.com/reference/stack/stack
+	*/
+	template<class T, class Container = ft::vector<T> >
+	class stack {
+			template<class _T, class _Container>
+			friend bool operator==(const stack<_T, _Container> &, const stack<_T, _Container> &);
+
+			template<class _T, class _Container>
+			friend bool operator<(const stack<_T, _Container> &, const stack<_T, _Container> &);
+
+		public:
+			typedef T			value_type;
+			typedef Container	container_type;
+			typedef std::size_t	size_type;
+		private:
+			container_type this->_items;
+		public:
+			/**
+			 * Construct stack
+			 * 
+			 * Constructs a stack container adaptor object.
+			 * 
+			 */
+			explicit stack(const container_type &container = container_type()) 
+				: this->_items( container ) { }
+			
+			/* stack copy constructor */
+			stack(const stack &s) : this->_items(s._items) {}
+
+			/* stack ssignation construct */
+			stack	&operator=(const stack &s) {
+				if ( &s != this )
+					this->_items = s.this->_items;
+				return *this;
+			}
+
+			virtual ~stack() { }
+			
+			/**
+			 * Return size
+			 * 
+			 * Returns the number of elements in the stack.
+			 * 
+			 * This member function effectively calls member size of the underlying container object.
+			 * 
+			 */
+			size_type size() const {
+				return this->_items.size();
+			}
+
+			/**
+			 * Test whether container is empty
+			 * 
+			 * Returns whether the stack is empty: i.e. whether its size is zero.
+			 * 
+			 * This member function effectively calls member empty of the underlying container object.
+			 * 
+			 */
+			bool empty() const {
+				return size() == 0;
+			}
+
+			/**
+			 * Access next element
+			 * 
+			 * Returns a reference to the top element in the stack.
+			 * 
+			 * Since stacks are last-in first-out containers, the top element is the last element inserted 
+			 * into the stack.
+			 * 
+			 * This member function effectively calls member back of the underlying container object
+			 * 
+			 */
+			value_type &top() {
+				return this->_items.back();
+			}
+
+			const value_type &top() const {
+				return this->_items.back();
+			}
+
+			/**
+			 * Insert element
+			 * 
+			 * Inserts a new element at the top of the stack, above its current top element. 
+			 * The content of this new element is initialized to a copy of val.
+			 * 
+			 * This member function effectively calls the member function push_back of the underlying container object.
+			 * 
+			 */
+			void push(const value_type &val){
+				this->_items.push_back(val);
+			}
+
+			/**
+			 * Remove top element
+			 * 
+			 * Removes the element on top of the stack, effectively reducing its size by one.
+			 * 
+			 * The element removed is the latest element inserted into the stack, whose value can be retrieved by calling member stack::top.
+			 * 
+			 * This calls the removed element's destructor.
+			 * 
+			 * This member function effectively calls the member function pop_back of the underlying container object.
+			 * 
+			 */
+			void pop() {
+				this->_items.pop_back();
+			}
 	};
+
+	template<class T, class Container>
+	bool operator==(const stack<T, Container> &x, const stack<T, Container> &y) {
+		return x.this->_items == y.this->_items;
+	}
+
+	template<class T, class Container>
+	bool operator<(const stack<T, Container> &x, const stack<T, Container> &y) {
+		return x.this->_items < y.this->_items;
+	}
+
+	template<class T, class Container>
+	bool operator!=(const stack<T, Container> &x, const stack<T, Container> &y){
+		return !(x == y);
+	}
+
+	template<class T, class Container>
+	bool operator>(const stack<T, Container> &x, const stack<T, Container> &y){
+        return y < x;
+	}
+
+	template<class T, class Container>
+	bool operator>=(const stack<T, Container> &x, const stack<T, Container> &y){
+        return !(x < y);
+	}
+
+	template<class T, class Container>
+	bool operator<=(const stack<T, Container> &x, const stack<T, Container> &y){
+        return !(y < x);
+	}
 }
 #endif
--- utils/algo.hpp
+++ utils/algo.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 15:54:45 by plam              #+#    #+#             */
-/*   Updated: 2022/12/21 14:40:14 by plam             ###   ########.fr       */
+/*   Updated: 2022/12/26 10:54:24 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,18 @@
 
 namespace ft
 {
-	/**
-	 * Lexicographical less-than comparison
-	 * 
-	 * Returns true if the range [first1,last1) compares lexicographically less than the range [first2,last2).
-	 * 
-	 * A lexicographical comparison is the kind of comparison generally used to sort 
-	 * words alphabetically in dictionaries; It involves comparing sequentially 
-	 * the elements that have the same position in both ranges against each other 
-	 * until one element is not equivalent to the other. The result of comparing 
-	 * these first non-matching elements is the result of the lexicographical comparison.
-	 * 
-	 * If both sequences compare equal until one of them ends, the shorter sequence is lexicographically
-	 * less than the longer one.
-	 * 
-	 * The elements are compared using operator< for the first version, and comp for the second.
-	 * Two elements, a and b are considered equivalent if (!(a<b) && !(b<a)) or if (!comp(a,b) && !comp(b,a)).
-	 * 
-	 * 
-	 */
+	/* Lexicographical less-than comparison :
+	** Returns true if the range [first1,last1) compares lexicographically less than the range [first2,last2).
+	** A lexicographical comparison is the kind of comparison generally used to sort
+	** words alphabetically in dictionaries; It involves comparing sequentially
+	** the elements that have the same position in both ranges against each other
+	** until one element is not equivalent to the other. The result of comparing
+	** these first non-matching elements is the result of the lexicographical comparison.
+	** If both sequences compare equal until one of them ends, the shorter sequence is lexicographically
+	** less than the longer one.
+	** The elements are compared using the operator< for the first version, and comp for the second.
+	** Two elements, a and b are considered equivalent if (!(a<b) && !(b<a)) or if (!comp(a,b) && !comp(b,a)).
+	*/
 	template<class InputIterator1, class InputIterator2>
 	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
 								 InputIterator2 first2, InputIterator2 last2) {
