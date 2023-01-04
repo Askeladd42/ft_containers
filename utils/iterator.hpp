9b38a1fa979cc4432f86c6f291289ffcc2d08070
Pierre-marie LAM <plam@e1r9p9.clusters.42paris.fr>
2023-01-04 11:14:20 +0100

    many issues with iterator operators, need fix and last testing before final push, modified some compilation flags

--- utils/iterator.hpp
+++ utils/iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 10:37:44 by plam              #+#    #+#             */
-/*   Updated: 2023/01/04 11:09:55 by plam             ###   ########.fr       */
+/*   Updated: 2023/01/04 11:14:13 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -203,12 +203,8 @@ namespace ft {
 				_current += n;
 				return *this;
 			}
-
-			inline reverse_iterator<Iter>
-			operator+(typename reverse_iterator<Iter>::difference_type n, reverse_iterator<Iter> const &i) {
-				return reverse_iterator(i->base() - n);
-			}
 	};
+
 	/* comparison operator functions recreation */
 	template<class Iter>
 	inline bool	operator==(reverse_iterator<Iter> const &x, reverse_iterator<Iter> const &y) {
@@ -244,6 +240,12 @@ namespace ft {
 	inline typename reverse_iterator<Iter>::difference_type
 	operator-(reverse_iterator<Iter> const &x, reverse_iterator<Iter> const &y) {
 		return (y.base() - x.base());
+	}
+
+	template<class Iter>
+	inline reverse_iterator<Iter>
+	operator+(typename reverse_iterator<Iter>::difference_type n, reverse_iterator<Iter> const &i) {
+		return reverse_iterator(i->base() - n);
 	}
 
 	/* Overload operator for reverse_iterator and const reverse_iterator comparison cases */
