818b8ff3ecb02dc5d020554a27eb77bfa54ff360
Pierre-marie LAM <plam@e1r9p9.clusters.42paris.fr>
2023-01-04 11:33:06 +0100

    issues with iterator operators solved, need fix for size_type issues in vector template and last testing before final push, modified some compilation flags

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 18:32:11 by plam              #+#    #+#             */
-/*   Updated: 2022/12/28 12:22:29 by plam             ###   ########.fr       */
+/*   Updated: 2023/01/04 11:33:02 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,20 @@
 namespace ft {
 	template< class T, class Allocator = std::allocator<T> >
 	class vector {
+		public:
+			typedef std::size_t										size_type;
+			typedef std::ptrdiff_t									difference_type;
+			typedef T												value_type;
+			typedef Allocator<value_type>							allocator_type;
+			typedef typename allocator_type::reference				reference;
+			typedef typename allocator_type::const_reference		const_reference;
+			typedef typename allocator_type::pointer				pointer;
+			typedef typename allocator_type::const_pointer			const_pointer;
+			typedef typename ft::normal_iterator<pointer>			iterator;
+			typedef typename ft::normal_iterator<const_pointer>		const_iterator;
+			typedef typename ft::reverse_iterator<iterator>			reverse_iterator;
+			typedef typename ft::reverse_iterator<const_iterator>	const_reverse_iterator;
+
 		private:
 			Allocator				_alloc;
 			size_type				_capacity;
@@ -60,19 +74,6 @@ namespace ft {
 			}
 
 		public:
-			typedef std::size_t										size_type;
-			typedef std::ptrdiff_t									difference_type;
-			typedef T												value_type;
-			typedef Allocator<value_type>							allocator_type;
-			typedef typename allocator_type::reference				reference;
-			typedef typename allocator_type::const_reference		const_reference;
-			typedef typename allocator_type::pointer				pointer;
-			typedef typename allocator_type::const_pointer			const_pointer;
-			typedef typename ft::normal_iterator<pointer>			iterator;
-			typedef typename ft::normal_iterator<const_pointer>		const_iterator;
-			typedef typename ft::reverse_iterator<iterator>			reverse_iterator;
-			typedef typename ft::reverse_iterator<const_iterator>	const_reverse_iterator;
-
 	/* default constructor(empty) */
 			explicit vector( const allocator_type &alloc = allocator_type() ) :
 				this->_alloc(alloc), this->_capacity(0),
